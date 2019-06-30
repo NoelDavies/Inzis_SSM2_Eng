1bdedd4fdd5f3df9a3dff086d58190daea1c4363
Daniel Noel-Davies <NoelDavies@cybershade.org>
2019-06-30 11:40:15 +0100

    Remove decompile comments

--- Settings.cs
+++ Settings.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Inzis_SMM2_Tool.Settings
-// Assembly: Inzis SMM2 Tool, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: B3400F75-7E13-41D1-8C16-0812E4AF9035
-// Assembly location: C:\Users\Danie\Downloads\Inzis_SMM2_Tool\Inzis SMM2 Tool.exe
-
-namespace Inzis_SMM2_Tool
+﻿namespace Inzis_SMM2_Tool
 {
   internal class Settings
   {
